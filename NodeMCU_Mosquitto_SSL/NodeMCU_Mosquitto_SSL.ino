53b5cafa0f8dc3fc0238d494d44c5464cb7dcd93
ivan-alfonso <ivandaalf@hotmail.com>
2021-07-23 10:55:34 -0500

    NodeMCU mosquitto SSL

---
+++ NodeMCU_Mosquitto_SSL/NodeMCU_Mosquitto_SSL.ino
@@ -0,0 +1,139 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <time.h>
+#include <PubSubClient.h>
+
+#include "secrets.h"
+
+const char ssid[] = "wifi-name";
+const char pass[] = "wifi-password";
+
+#define HOSTNAME "nodeMCU-hostname"
+
+const char MQTT_HOST[] = "ip-mosquitto-broker";
+const int MQTT_PORT = 8883;
+const char MQTT_USER[] = "mosquitto-user"; // leave blank if no credentials used
+const char MQTT_PASS[] = "mosquitto-password"; // leave blank if no credentials used
+const char MQTT_SUB_TOPIC[] = HOSTNAME "/";
+const char MQTT_PUB_TOPIC[] = HOSTNAME "/city/temperature";
+
+//////////////////////////////////////////////////////
+
+#if (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_FINGERPRINT)) or (defined(CHECK_FINGERPRINT) and defined(CHECK_CA_ROOT)) or (defined(CHECK_PUB_KEY) and defined(CHECK_CA_ROOT) and defined(CHECK_FINGERPRINT))
+  #error "cant have both CHECK_CA_ROOT and CHECK_PUB_KEY enabled"
+#endif
+
+BearSSL::WiFiClientSecure net;
+PubSubClient client(net);
+
+time_t now;
+unsigned long lastMillis = 0;
+
+void mqtt_connect()
+{
+  while (!client.connected()) {
+    Serial.print("Time: ");
+    Serial.print(ctime(&now));
+    Serial.print("MQTT connecting ... ");
+    if (client.connect(HOSTNAME, MQTT_USER, MQTT_PASS)) {
+      Serial.println("connected.");
+      //client.subscribe(MQTT_SUB_TOPIC);
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println(". Try again in 5 seconds.");
+      /* Wait 5 seconds before retrying */
+      delay(5000);
+    }
+  }
+}
+
+void receivedCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.print(ssid);
+  WiFi.hostname(HOSTNAME);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("connected!");
+
+  Serial.print("Setting time using SNTP");
+  configTime(-5 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  now = time(nullptr);
+  while (now < 1510592825) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("done!");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+
+  #ifdef CHECK_CA_ROOT
+    BearSSL::X509List cert(digicert);
+    net.setTrustAnchors(&cert);
+  #endif
+  #ifdef CHECK_PUB_KEY
+    BearSSL::PublicKey key(pubkey);
+    net.setKnownKey(&key);
+  #endif
+  #ifdef CHECK_FINGERPRINT
+    net.setFingerprint(fp);
+  #endif
+  #if (!defined(CHECK_PUB_KEY) and !defined(CHECK_CA_ROOT) and !defined(CHECK_FINGERPRINT))
+    net.setInsecure();
+  #endif
+
+  client.setServer(MQTT_HOST, MQTT_PORT);
+  client.setCallback(receivedCallback);
+  mqtt_connect();
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Checking wifi");
+    while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, pass);
+      Serial.print(".");
+      delay(10);
+    }
+    Serial.println("connected");
+  }
+  else
+  {
+    if (!client.connected())
+    {
+      mqtt_connect();
+    }
+    else
+    {
+      client.loop();
+    }
+  }
+
+  now = time(nullptr);
+  client.publish(MQTT_PUB_TOPIC, ctime(&now), false);
+  delay(5000);
+}
---
+++ NodeMCU_Mosquitto_SSL/secrets.h
@@ -0,0 +1,63 @@
+//enable only one of these below, disabling both is fine too.
+#define CHECK_CA_ROOT
+// #define CHECK_PUB_KEY
+// #define CHECK_FINGERPRINT
+////--------------------------////
+
+#define SECRET
+
+#ifdef CHECK_CA_ROOT
+static const char digicert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+-----END CERTIFICATE-----
+)EOF";
+#endif
+
+#ifdef CHECK_PUB_KEY
+// Extracted by: openssl x509 -pubkey -noout -in ca.crt
+static const char pubkey[] PROGMEM = R"KEY(
+-----BEGIN PUBLIC KEY-----
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxx
+-----END PUBLIC KEY-----
+)KEY";
+#endif
+
+#ifdef CHECK_FINGERPRINT
+// Extracted by: openssl x509 -fingerprint -in server.crt
+static const char fp[] PROGMEM = "AA:BB:CC:DD:EE:FF:00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD";
+#endif
