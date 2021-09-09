0d2c3fdb47adfcedf1c6e3bd345c523945018184
Nestor <nf.gonzalez@uniandes.edu.co>
2021-09-08 22:56:03 -0500

    Comentarios código y corrección mensaje WiFi

--- NodeMCU_Mosquitto_SSL/NodeMCU_Mosquitto_SSL.ino
+++ NodeMCU_Mosquitto_SSL/NodeMCU_Mosquitto_SSL.ino
@@ -10,18 +10,27 @@ DHT dht(dht_dpin, DHTTYPE);
 
 #include "secrets.h"
 
+//Conexión a Wifi
+//Nombre de la red Wifi
 const char ssid[] = "wifi-name";
+//Contraseña de la red Wifi
 const char pass[] = "wifi-password";
 
+//Usuario uniandes sin @uniandes.edu.co
 #define HOSTNAME "nodeMCU-hostname"
 
+//Conexión a Mosquitto
 const char MQTT_HOST[] = "iotlab.virtual.uniandes.edu.co";
 const int MQTT_PORT = 8082;
-const char MQTT_USER[] = "mosquitto-user"; // leave blank if no credentials used
-const char MQTT_PASS[] = "mosquitto-password"; // leave blank if no credentials used
+//Usuario uniandes sin @uniandes.edu.co
+const char MQTT_USER[] = "mosquitto-user";
+//Contraseña de MQTT que recibió por correo
+const char MQTT_PASS[] = "mosquitto-password";
 const char MQTT_SUB_TOPIC[] = HOSTNAME "/";
-const char MQTT_PUB_TOPIC1[] = "moisture/city/" HOSTNAME;
-const char MQTT_PUB_TOPIC2[] = "temperature/city/" HOSTNAME;
+//Tópico al que se enviarán los datos de humedad
+const char MQTT_PUB_TOPIC1[] = "humedad/ciudad/" HOSTNAME;
+//Tópico al que se enviarán los datos de temperatura
+const char MQTT_PUB_TOPIC2[] = "temperatura/ciudad/" HOSTNAME;
 
 //////////////////////////////////////////////////////
 
@@ -35,20 +44,22 @@ PubSubClient client(net);
 time_t now;
 unsigned long lastMillis = 0;
 
+//Función que conecta el node a través del protocolo MQTT
+//Emplea los datos de usuario y contraseña definidos en MQTT_USER y MQTT_PASS para la conexión
 void mqtt_connect()
 {
+  //Intenta realizar la conexión indefinidamente hasta que lo logre
   while (!client.connected()) {
     Serial.print("Time: ");
     Serial.print(ctime(&now));
     Serial.print("MQTT connecting ... ");
     if (client.connect(HOSTNAME, MQTT_USER, MQTT_PASS)) {
       Serial.println("connected.");
-      //client.subscribe(MQTT_SUB_TOPIC);
     } else {
       Serial.print("failed, status code =");
       Serial.print(client.state());
       Serial.println(". Try again in 5 seconds.");
-      /* Wait 5 seconds before retrying */
+      /* Espera 5 segundos antes de volver a intentar */
       delay(5000);
     }
   }
@@ -63,6 +74,7 @@ void receivedCallback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
+//Configura la conexión del node MCU a Wifi y a Mosquitto
 void setup()
 {
   Serial.begin(115200);
@@ -73,13 +85,12 @@ void setup()
   WiFi.hostname(HOSTNAME);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
+  //Intenta conectarse con los valores de las constantes ssid y pass a la red Wifi
+  //Si la conexión falla el node se dormirá hasta que lo resetee
   while (WiFi.status() != WL_CONNECTED)
   {
-    if ( WiFi.status() == WL_NO_SSID_AVAIL ) {
-      Serial.print("\nLa red WiFi no se ha encontrado, revise que haya escrito correctamente el nombre de la red en el valor de la constante ssid");
-      ESP.deepSleep(0);
-    } else if ( WiFi.status() == WL_WRONG_PASSWORD ) {
-      Serial.print("\nLa contraseña ingresada no es correcta, revise que haya escrito correctamente la contraseña de la red en el valor de la constante pass");
+    if ( WiFi.status() == WL_NO_SSID_AVAIL || WiFi.status() == WL_WRONG_PASSWORD ) {
+      Serial.print("\nProblema con la conexión, revise los valores de las constantes ssid y pass");
       ESP.deepSleep(0);
     } else if ( WiFi.status() == WL_CONNECT_FAILED ) {
       Serial.print("\nNo se ha logrado conectar con la red, resetee el node y vuelva a intentar");
@@ -90,6 +101,7 @@ void setup()
   }
   Serial.println("connected!");
 
+  //Sincroniza la hora del dispositivo con el servidor SNTP (Simple Network Time Protocol)
   Serial.print("Setting time using SNTP");
   configTime(-5 * 3600, 0, "pool.ntp.org", "time.nist.gov");
   now = time(nullptr);
@@ -101,6 +113,7 @@ void setup()
   Serial.println("done!");
   struct tm timeinfo;
   gmtime_r(&now, &timeinfo);
+  //Una vez obtiene la hora, imprime en el monitor el tiempo actual
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
 
@@ -119,13 +132,18 @@ void setup()
     net.setInsecure();
   #endif
 
+  //Llama a funciones de la librería PubSubClient para configurar la conexión con Mosquitto
   client.setServer(MQTT_HOST, MQTT_PORT);
   client.setCallback(receivedCallback);
+  //Llama a la función de este programa que realiza la conexión con Mosquitto
   mqtt_connect();
 }
 
+//Función loop que se ejecuta indefinidamente repitiendo el código a su interior
+//Cada vez que se ejecuta toma nuevos datos de la lectura del sensor
 void loop()
 {
+  //Revisa que la conexión Wifi y MQTT siga activa
   if (WiFi.status() != WL_CONNECTED)
   {
     Serial.print("Checking wifi");
@@ -150,23 +168,36 @@ void loop()
   }
 
   now = time(nullptr);
+  //Lee los datos del sensor
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+  //Transforma la información a la notación JSON para poder enviar los datos 
+  //El mensaje que se envía es de la forma {"value": x}, donde x es el valor de temperatura o humedad
+  
+  //JSON para humedad
   String json = "{\"value\": "+ String(h) + "}";
   char payload1[json.length()+1];
   json.toCharArray(payload1,json.length()+1);
+  //JSON para temperatura
   json = "{\"value\": "+ String(t) + "}";
   char payload2[json.length()+1];
   json.toCharArray(payload2,json.length()+1);
+
+  //Si los valores recolectados no son indefinidos, se envían a los tópicos correspondientes
   if ( !isnan(h) && !isnan(t) ) {
+    //Publica en el tópico de la humedad
     client.publish(MQTT_PUB_TOPIC1, payload1, false);
+    //Publica en el tópico de la temperatura
     client.publish(MQTT_PUB_TOPIC2, payload2, false);
   }
+
+  //Imprime en el monitor serial la información recolectada
   Serial.print(MQTT_PUB_TOPIC1);
   Serial.print(" -> ");
   Serial.println(payload1);
   Serial.print(MQTT_PUB_TOPIC2);
   Serial.print(" -> ");
-  Serial.println(payload2); 
+  Serial.println(payload2);
+  /*Espera 5 segundos antes de volver a ejecutar la función loop*/
   delay(5000);
 }
