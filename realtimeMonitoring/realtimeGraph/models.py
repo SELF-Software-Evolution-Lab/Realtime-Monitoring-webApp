86aa542a73c3f81d2fc8700418f691e818be978c
ja-avos <javeino2311@gmail.com>
2022-01-11 13:33:26 -0500

    Organización y documentación de código

--- realtimeMonitoring/realtimeGraph/models.py
+++ realtimeMonitoring/realtimeGraph/models.py
@@ -27,19 +27,46 @@ class User(models.Model):
     def str(self):
         return '{}'.format(self.login)
 
-
 class City(models.Model):
     name = models.CharField(max_length=50, unique=True, blank=False)
-    description = models.CharField(max_length=200, blank=True)
-    lat = models.DecimalField(
-        max_digits=9, decimal_places=6, null=True, blank=True)
-    lon = models.DecimalField(
-        max_digits=9, decimal_places=6, null=True, blank=True)
-    active = models.BooleanField(default=True)
+    code = models.CharField(max_length=50, null=True)
 
     def str(self):
         return '{}'.format(self.name)
 
+class State(models.Model):
+    name = models.CharField(max_length=50, unique=False, blank=False)
+    code = models.CharField(max_length=50, null=True)
+
+    def str(self):
+        return '{}'.format(self.name)
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, unique=False, blank=False)
+    code = models.CharField(max_length=50, null=True)
+
+    def str(self):
+        return '{}'.format(self.name)
+
+class Location(models.Model):
+    description = models.CharField(max_length=200, blank=True)
+    lat = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
+    lng = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, default=USER_ROLE_ID)
+    state = models.ForeignKey(
+        State, on_delete=models.CASCADE, default=USER_ROLE_ID)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, default=USER_ROLE_ID)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('city', 'state', 'country')
+
+    def str(self):
+        return '{}'.format(self.name)
 
 class Measurement(models.Model):
     name = models.CharField(max_length=50, blank=False)
@@ -53,18 +80,18 @@ class Measurement(models.Model):
 
 class Station(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, default=None)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
     class Meta:
-        unique_together = ('user', 'city', 'measurement',)
+        unique_together = ('user', 'location')
     last_activity = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
     def str(self):
-        return '%s %s %s' % (self.user, self.city, self.last_activity)
+        return '%s %s %s' % (self.user, self.location, self.last_activity)
 
 class Data(models.Model):
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
     value = models.FloatField(blank=False)
     time = models.DateTimeField(primary_key=True, default=timezone.now)
 
@@ -85,11 +112,12 @@ class Data(models.Model):
                 self.save_and_smear_timestamp(*args, **kwargs)
 
     def str(self):
-        return '{} {}'.format(self.value, self.time)
+        return '{} {} {} {}'.format(self.value, self.time, self.station, self.measurement)
 
     def toDict(self):
         return {
             'station': str(self.station),
+            'measurement': str(self.measurement),
             'value': self.value,
-            'created_at': self.time
+            'time': self.time
         }
--- realtimeMonitoring/realtimeMonitoring/urls.py
+++ realtimeMonitoring/realtimeMonitoring/urls.py
@@ -1,27 +1,29 @@
-"""realtimeMonitoring URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from . import mqtt
-from . import utils
+# from . import mqtt
+# from . import utils
 
-utils.register_users()
-#utils.writeDataCSVFile()# No descomentar. Genera el archivo de todos los registros a la fecha. No corre con GUnicorn, se debe correr con python mnage.py runserver. Toma tiempo: para 200k datos tomó aprox. 10 min.
+# Función que registra usuarios en la base de datos. Los datos los toma del archivo users.pwd
+# que está en la carpeta raiz del proyecto.
+# utils.register_users()
+
+# Las siguientes 2 funciones se encargan de crear y actualizar el archivo CSV para descarga desde la página web
+# No descomentar. Genera el archivo de todos los registros a la fecha. No corre con GUnicorn, se debe correr con python mnage.py runserver. Toma tiempo: para 200k datos tomó aprox. 10 min.
+#utils.writeDataCSVFile()
+
+# Actualiza el archivo creado con la función anterior. Este ejecuta un cron cada 30 min para la actualización del archivo.
 #utils.updateCSVFile()
-mqtt.client.loop_start()
+
+# Función que carga a la base de datos el CSV generado por las funciones anteriores.
+# utils.loadCSV()
+
+# Si se quieren generar datos de prueba, descomentar la siguiente función.
+# 
+#utils.generateMockData()
+
+# La siguiente línea se encarga de iniciar el proceso de MQTT y escuchar los mensajes.
+# Descomentar para almacenar y registrar los mensajes que se publiquen desde las estaciones. 
+#mqtt.client.loop_start()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
