3fc232552d844fd09681ab8e18fbd208cb81bc20
Juan Avelino <juan.avelino@imagineapps.co>
2022-03-08 14:35:15 -0500

    Agregado endpoint de JSON

--- realtimeMonitoring/realtimeGraph/urls.py
+++ realtimeMonitoring/realtimeGraph/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('historical/', HistoricalView.as_view(), name='historical'),
     path('rema/', RemaView.as_view(), name='rema'),
     path('rema/<str:measure>', RemaView.as_view(), name='rema'),
+    path("mapJson/", get_map_json, name="mapJson"),
+    path("mapJson/<str:measure>", get_map_json, name="mapJson"),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('historical/data',
--- realtimeMonitoring/realtimeGraph/views.py
+++ realtimeMonitoring/realtimeGraph/views.py
@@ -490,6 +490,102 @@ class RemaView(TemplateView):
         return context
 
 
+"""
+Se procesan los datos para enviar en JSON
+La respuesta tiene esta estructura:
+{
+    "data": [
+        {
+            "name": "ciudad, estado, país",
+            "lat": float,
+            "lng": float,
+            "population": int,
+            "min": float,
+            "max": float,
+            "avg": float
+        },
+        {name, lat, lng, pop, min, max, avg},
+        {name, lat, lng, pop, min, max, avg},
+        ...
+    ],
+    "measurements": [Measurement0, Measurement1, ...],
+    "selectedMeasure": Measurement,
+    "locations": [Location0, Location1, ...],
+    "start": startTime,
+    "end": endTime
+}
+"""
+
+
+def get_map_json(request, **kwargs):
+    data_result = {}
+
+    measureParam = kwargs.get("measure", None)
+    selectedMeasure = None
+    measurements = Measurement.objects.all()
+
+    if measureParam != None:
+        selectedMeasure = Measurement.objects.filter(name=measureParam)[0]
+    elif measurements.count() > 0:
+        selectedMeasure = measurements[0]
+
+    locations = Location.objects.all()
+    try:
+        start = datetime.fromtimestamp(
+            float(request.GET.get("from", None)) / 1000
+        )
+    except:
+        start = None
+    try:
+        end = datetime.fromtimestamp(
+            float(request.GET.get("to", None)) / 1000)
+    except:
+        end = None
+    if start == None and end == None:
+        start = datetime.now()
+        start = start - dateutil.relativedelta.relativedelta(weeks=1)
+        end = datetime.now()
+        end += dateutil.relativedelta.relativedelta(days=1)
+    elif end == None:
+        end = datetime.now()
+    elif start == None:
+        start = datetime.fromtimestamp(0)
+
+    data = []
+
+    for location in locations:
+        stations = Station.objects.filter(location=location)
+        locationData = Data.objects.filter(
+            station__in=stations, measurement__name=selectedMeasure.name,  time__gte=start.date(), time__lte=end.date())
+        if locationData.count() <= 0:
+            continue
+        minVal = locationData.aggregate(
+            Min('value'))['value__min']
+        maxVal = locationData.aggregate(
+            Max('value'))['value__max']
+        avgVal = locationData.aggregate(
+            Avg('value'))['value__avg']
+        data.append({
+            'name': f'{location.city.name}, {location.state.name}, {location.country.name}',
+            'lat': location.lat,
+            'lng': location.lng,
+            'population': stations.count(),
+            'min': minVal if minVal != None else 0,
+            'max': maxVal if maxVal != None else 0,
+            'avg': round(avgVal if avgVal != None else 0, 2),
+        })
+
+    startFormatted = start.strftime("%d/%m/%Y") if start != None else " "
+    endFormatted = end.strftime("%d/%m/%Y") if end != None else " "
+
+    data_result["locations"] = [loc.str() for loc in locations]
+    data_result["start"] = startFormatted
+    data_result["end"] = endFormatted
+    data_result["data"] = data
+
+    return JsonResponse(data_result)
+
+
 def download_csv_data(request):
     print("Getting time for csv req")
     startT = time.time()
