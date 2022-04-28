8bbbe3746f89a60e878b1c5308e036c1c2b4f827
Juan Avelino <juan.avelino@imagineapps.co>
2022-04-28 14:48:35 -0500

    Cambio de variables

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "ip.maquina.visualizador"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.238.40.3",  # Dirección IP de la base de datos
+        "HOST": "ip.maquina.db",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
