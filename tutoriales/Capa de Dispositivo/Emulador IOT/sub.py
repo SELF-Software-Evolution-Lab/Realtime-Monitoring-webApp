999fcf86166a6f688154eb4ca05bc8ae7dedb237
Juan Avelino <juan.avelino@imagineapps.co>
2022-06-09 16:16:50 -0500

    Actualización de código emulador

--- tutoriales/Capa de Dispositivo/Emulador IOT/sub.py
+++ tutoriales/Capa de Dispositivo/Emulador IOT/sub.py
@@ -9,9 +9,9 @@ import argparse
 parser = argparse.ArgumentParser(description='IOT Sensor Emulator')
 parser.add_argument("--host", type=str,
                     default="iotlab.virtual.uniandes.edu.co", help="MQTT Host")
-# parser.add_argument("--user", type=str, required=True, help="MQTT User")
-# parser.add_argument("--passwd", type=str, required=True, help="MQTT Password")
-# parser.add_argument("--city", type=str, required=True, help="MQTT City")
+parser.add_argument("--user", type=str, required=True, help="MQTT User")
+parser.add_argument("--passwd", type=str, required=True, help="MQTT Password")
+parser.add_argument("--city", type=str, required=True, help="MQTT City")
 
 args = parser.parse_args()
 
@@ -52,7 +52,7 @@ def on_log(client, userdata, level, buf):
 client.tls_set(ca_certs='ca.crt',
                tls_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_NONE)
 print("TLS Done")
-# client.username_pw_set(args.user, args.passwd)
+client.username_pw_set(args.user, args.passwd)
 # client.on_publish = on_publish
 client.on_message = on_message
 client.on_subscribe = on_subscribe
